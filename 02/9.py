d6ec1f07ac38ce78f36e9d159cfc14db6e0520bd
Michaela Taubingerová <taubingerova@gmail.com>
2020-06-03 09:26:35 +0200

    Pokus o přidání úkolu 9 z lekce 02-

---
+++ 02/9.py
@@ -0,0 +1,14 @@
+from random import randrange
+cislo = randrange(3)
+print(cislo)
+tah_pocitace = ('kámen') or ('nůžky') or ('papír')
+if cislo == 1:
+    tah_pocitace == 'nůžky'
+    print('nůžky')
+elif cislo == 0:
+    tah_pocitace ='kámen'
+    print('kámen')
+else: tah_pocitace = print('papír')
+   
+    
+
